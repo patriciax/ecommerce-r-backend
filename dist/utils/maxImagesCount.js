766ebc4af478f675f612d5608933796a23937e0b
Willian Rodriguez <willian@amobasoftware.com>
2024-02-07 19:16:14 -0400

    removed compilation

--- dist/utils/maxImagesCount.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.maxImagesCount = void 0;
-exports.maxImagesCount = process.env.MAX_IMAGES_COUNT ? parseInt(process.env.MAX_IMAGES_COUNT) : 3;
