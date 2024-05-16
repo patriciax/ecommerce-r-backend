195c4916cae2d2b5b9ea199899584437029f9dd1
Willian Jesus Rodriguez Ramirez <willian@amobasoftware.com>
2024-05-16 16:22:28 -0400

    statistics

--- src/controllers/statisticController.ts
+++ src/controllers/statisticController.ts
@@ -118,80 +118,46 @@ export class StatisticController {
         let endOfDay = new Date();
         endOfDay.setHours(23, 59, 59, 999);
 
-        const bolivarsRevenues:any = await Payment.find([
-            {type: ['pagoMovil', 'banesco']},
+        const bolivarsRevenues:any = await Payment.find(
             {
-                $gte: startOfDay, 
-                $lt: endOfDay
+                type: { 
+                    $in : ["pagoMovil", "banesco"]
+                },
+                created: {
+                    $gte: startOfDay, 
+                    $lt: endOfDay
+                }
             }
-        ])
-
-        return bolivarsRevenues.reduce((acc, curr) => {
-
-            
-
-        }, 0)
+        )
+        
+        return bolivarsRevenues.reduce((acc: number, payment: any) => acc + payment.total + payment.taxAmount, 0)
     }
 
     private todayDolars = async () => {
-        const dolarsRevenuesTotal:any = await Payment.aggregate(
-            [
-                {
-                    $match: { 
-                        type: { 
-                            $in : ["paypal", "zelle", "giftCard"]
-                        } 
-                    }
-                },
-                {
-                    $group: { 
-                        _id: null, 
-                        totalQuantity: { $sum: '$total' }
-                    }
-                }
-            ]
 
+        let startOfDay = new Date();
+        startOfDay.setHours(0, 0, 0, 0);
+        
+        let endOfDay = new Date();
+        endOfDay.setHours(23, 59, 59, 999);
+
+        const dolarsRevenue:any = await Payment.find(
+            {
+                type: { 
+                    $in : ["paypal", "zelle", "giftCard"]
+                },
+                
+                created: {
+                    $gte: startOfDay, 
+                    $lt: endOfDay
+                }
+                
+            
+            }
         )
 
-        const dolarsRevenuesTaxAmount:any = await Payment.aggregate(
-            [
-                {
-                    $match: { 
-                        type: { 
-                            $in : ["paypal", "zelle", "giftCard"]
-                        } 
-                    }
-                },
-                {
-                    $group: { 
-                        _id: null, 
-                        totalQuantity: { $sum: '$taxAmount' } 
-                    }
-                }
-            ]
+        return dolarsRevenue.reduce((acc: number, payment: any) => acc + payment.total + payment.taxAmount + payment.createRate.amount, 0)
 
-        )
-
-        const dolarsRevenuesCarrier:any = await Payment.aggregate(
-            [
-                {
-                    $match: { 
-                        type: { 
-                            $in : ["paypal", "zelle", "giftCard"]
-                        } 
-                    }
-                },
-                {
-                    $group: { 
-                        _id: null, 
-                        totalQuantity: { $sum: '$carrierRateAmount' } 
-                    }
-                }
-            ]
-
-        )
-
-        return dolarsRevenuesTotal[0]?.totalQuantity + dolarsRevenuesTaxAmount[0]?.totalQuantity + dolarsRevenuesCarrier[0]?.totalQuantity
     }
 
     public getClients = async () => {
@@ -231,11 +197,13 @@ export class StatisticController {
         const totalDolars = await this.dolars()
         const clients = await this.getClients()
         const latestSales = await this.latestSales()
-
+        const todayBolivars = await this.todayBolivars()
+        const todayDolars = await this.todayDolars()
         
-        console.log(totalBolivarsDolarPrice + totalDolars)
-        console.log(clients)
-        console.log(latestSales)
+        //console.log(totalBolivarsDolarPrice + totalDolars)
+        //console.log(clients)
+        //console.log(latestSales)
+        console.log(todayBolivars)
         //await Payment.find({"type" : { $in : ["pagoMovil", "banesco"]}}).
 
         res.status(200).json({
