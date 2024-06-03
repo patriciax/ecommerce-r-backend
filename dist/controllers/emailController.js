12a8434bb5b5897597558e2492cca06263c4d8e4
Willian Jesus Rodriguez Ramirez <willian@amobasoftware.com>
2024-06-03 17:09:47 -0400

    compile

--- dist/controllers/emailController.js
+++ dist/controllers/emailController.js
@@ -36,7 +36,7 @@ class EmailController {
         this.transporter = (0, nodemailer_1.createTransport)({
             host: process.env.EMAIL_HOST,
             port: parseInt(process.env.EMAIL_PORT || '0'),
-            secure: true,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
